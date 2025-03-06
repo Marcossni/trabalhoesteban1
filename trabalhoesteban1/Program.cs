d664031c737030f4c475deb1cd6b7cc68669ca8d
Marcossni <marcos.pedromartins02@gmail.com>
2025-03-06 18:52:02 -0300

    Program.cs

--- trabalhoesteban1/Program.cs
+++ trabalhoesteban1/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace Exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Digite um número inteiro para mostrar todos os seus divisores positivos: ");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
